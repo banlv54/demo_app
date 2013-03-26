abf30f94797fd119d99b7a0e7506089f1a503a00
Ban <banlv54@gmail.com>
2013-03-26 16:13:06 +0700

    final chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,12 @@ DemoApp::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/contact', to: 'static_pages#contact'
-
-  get "static_pages/home"
+  match '/help', to: 'static_pages#help'
 
   match '/signup', to: 'users#new'
   match '/signin', to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
-  get "static_pages/help"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
