dfa4fc72cd4006824a7c9094030101c21b8dcb9c
Ban <banlv54@gmail.com>
2013-03-29 13:38:05 +0700

    Finsh Micro

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@
 class User < ActiveRecord::Base 
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
+
   before_save{ |user| user.email = email.downcase }
   before_save :create_remember_token
   validates :name,  presence: true, length: { maximum: 50 }
@@ -17,6 +19,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX},
   	uniqueness: {case_sensitive: false}
 
+    def feed
+      Micropost.where("user_id = ?", id)
+    end
+    
   private
   	def create_remember_token
   		self.remember_token = SecureRandom.urlsafe_base64
